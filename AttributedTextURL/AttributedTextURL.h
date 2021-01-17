32de934fb8237c09d5a386a3079bd6b127622640
Jade Silveira <jadecs8@gmail.com>
2021-01-17 16:50:53 -0300

    Initial Commit

---
+++ AttributedTextURL/AttributedTextURL.h
@@ -0,0 +1,18 @@
+//
+//  AttributedTextURL.h
+//  AttributedTextURL
+//
+//  Created by Jade Silveira on 17/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AttributedTextURL.
+FOUNDATION_EXPORT double AttributedTextURLVersionNumber;
+
+//! Project version string for AttributedTextURL.
+FOUNDATION_EXPORT const unsigned char AttributedTextURLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttributedTextURL/PublicHeader.h>
+
+
