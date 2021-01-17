d0f5de46dbe472264364ccf8a56079db69dc5131
Jade Silveira <jadecs8@gmail.com>
2021-01-17 17:03:32 -0300

    Initial project setup

---
+++ AttributedTextURLExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AttributedTextURLExamples
+//
+//  Created by Jade Silveira on 17/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
